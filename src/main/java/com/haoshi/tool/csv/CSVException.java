22023f22b26a430960f973e85ff7b4a3f5e47a96
Bosto <287489004@qq.com>
2017-12-22 15:08:34 +0800

    Update CSVException.java

--- src/main/java/com/haoshi/tool/csv/CSVException.java
+++ src/main/java/com/haoshi/tool/csv/CSVException.java
@@ -1,26 +1,9 @@
-/*
- * COPYRIGHT. HSBC HOLDINGS PLC 2017. ALL RIGHTS RESERVED.
- *
- * This software is only to be used for the purpose for which it has been
- * provided. No part of it is to be reproduced, disassembled, transmitted,
- * stored in a retrieval system nor translated in any human or computer
- * language in any way or for any other purposes whatsoever without the
- * prior written consent of HSBC Holdings plc.
- */
 package com.haoshi.tool.csv;
 
-/**
- * <p><b>
- * TODO : Insert description of the class's responsibility/role.
- * </b></p>
- */
+
 public class CSVException extends Exception {
 
-    /**
-     * <p><b>
-     * TODO : Insert description of the field.
-     * </b></p>
-     */
+
     private static final long serialVersionUID = 6772269503534776867L;
 
     public CSVException(String message) {
